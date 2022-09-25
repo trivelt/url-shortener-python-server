f6dac773e016fc7038b509ab91644ac18d278573
Maciej Michalec <maciej.michalec@hpe.com>
2022-09-25 20:07:41 +0200

    Add short urls generator

--- app/shortener/generator.py
+++ app/shortener/generator.py
@@ -1,2 +1,42 @@
-def generate_short_url(long_url: str) -> str:
-    return long_url[:2]
+from app.shortener.models import URL
+
+INDEX_OFFSET = 10_000
+
+
+def generate_short_url() -> str:
+    index = URL.objects.count()
+    return Base62Converter.encode(index + INDEX_OFFSET)
+
+
+def get_index_from_short_url(url: str) -> int:
+    return Base62Converter.decode(url) - INDEX_OFFSET
+
+
+class Base62Converter:
+    ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ALPHABET_MAP = {character: index for index, character in enumerate(ALPHABET)}
+    BASE = len(ALPHABET)
+
+    @staticmethod
+    def encode(number: int) -> str:
+        if number == 0:
+            return Base62Converter.ALPHABET[0]
+
+        result = ""
+        while number:
+            result = Base62Converter.ALPHABET[number % Base62Converter.BASE] + result
+            number //= Base62Converter.BASE
+
+        return result
+
+    @staticmethod
+    def decode(short_url: str) -> int:
+        result = 0
+        index = 0
+
+        for char in short_url:
+            power = (len(short_url) - (index + 1))
+            result += Base62Converter.ALPHABET_MAP[char] * (Base62Converter.BASE ** power)
+            index += 1
+
+        return result
---
+++ app/shortener/test/test_generator.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+from app.shortener.models import URL
+from app.shortener.generator import Base62Converter, generate_short_url, get_index_from_short_url
+
+
+class TestShortURLGenerator(TestCase):
+    def test_all(self):
+        r = Base62Converter.encode(0)
+        self.assertEqual(r, '0')
+
+        r = Base62Converter.encode(30000)
+        self.assertEqual(r, '7NS')
+
+        r = generate_short_url()
+        self.assertEqual(r, '2Bi')
+
+    def test_al2l2(self):
+        r = Base62Converter.encode(0)
+        self.assertEqual(r, '0')
+
+        r = Base62Converter.encode(30000)
+        self.assertEqual(r, '7NS')
+
+        new_urls = [URL(long=f"XDD{i}", short=f"XD{i}") for i in range(2000)]
+        URL.objects.bulk_create(new_urls)
+
+        r = generate_short_url()
+        self.assertEqual(r, '37y')
+
+    def test_al3(self):
+        r = Base62Converter.decode("7NS")
+        self.assertEqual(r, 30000)
+
+        r = Base62Converter.decode('0')
+        self.assertEqual(r, 0)
+
+        r = Base62Converter.decode('2Bi')
+        self.assertEqual(r, 10000)
+
+        r = Base62Converter.decode('37y')
+        self.assertEqual(r, 12000)
+
+        r = get_index_from_short_url("37y")
+        self.assertEqual(r, 2000)
